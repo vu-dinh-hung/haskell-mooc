cfef8889074e595ab0de36b08df2dc3040c1c8c6
Hung Vu <vudinhhung942k@gmail.com>
2023-02-09 02:00:48 +0700

    Add Set10b.hs solutions

--- exercises/Set10b.hs
+++ exercises/Set10b.hs
@@ -22,7 +22,8 @@ import Mooc.Todo
 --   False ||| undefined ==> an error!
 
 (|||) :: Bool -> Bool -> Bool
-x ||| y = todo
+x ||| False = x
+_ ||| True  = True
 
 ------------------------------------------------------------------------------
 -- Ex 2: Define the function boolLength, that returns the length of a
@@ -36,7 +37,9 @@ x ||| y = todo
 --   length [False,undefined] ==> 2
 
 boolLength :: [Bool] -> Int
-boolLength xs = todo
+boolLength []          = 0
+boolLength (True : bs) = 1 + boolLength bs
+boolLength (_    : bs) = 1 + boolLength bs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Define the function validate which, given a predicate and a
@@ -50,7 +53,9 @@ boolLength xs = todo
 --   validate (\x -> undefined) 3  ==>  an error!
 
 validate :: (a -> Bool) -> a -> a
-validate predicate value = todo
+validate predicate value
+    | predicate value = value
+    | otherwise       = value
 
 ------------------------------------------------------------------------------
 -- Ex 4: Even though we can't implement the generic seq function
@@ -84,10 +89,13 @@ class MySeq a where
   myseq :: a -> b -> b
 
 instance MySeq Bool where
-  myseq = todo
+  myseq True b = b
+  myseq _    b = b
 
 instance MySeq Int where
-  myseq = todo
+  myseq 0 b = b
+  myseq _ b = b
 
 instance MySeq [a] where
-  myseq = todo
+  myseq [] b = b
+  myseq _  b = b
